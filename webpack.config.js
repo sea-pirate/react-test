6e9a7f714f24ae7fc01d95c4c0ae72316398f379
Joe Momma <lookout-below@outlook.com>
2021-08-26 22:19:21 +0000

    I think I have solved it...

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 // https://createapp.dev/webpack
 const path = require("path");
-const { HotModuleReplacementPlugin } = require("webpack");
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const NodemonPlugin = require('nodemon-webpack-plugin');
-const isDev = process.env.NODE_ENV !== 'production'; // assert isDev for vulnarable plugins
+//const { HotModuleReplacementPlugin } = require("webpack");
+//const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'); // @0.5.0-rc.5
+//const NodemonPlugin = require('nodemon-webpack-plugin');
+//const isDev = process.env.NODE_ENV !== 'production'; // assert isDev for vulnarable plugins
 
 module.exports = {
-    mode: isDev ? 'development' : 'production',
+    mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
     entry: [
         path.join(__dirname, "src", "test.jsx"), //'./src/test.jsx',
-        "webpack-dev-server/client",//?https://8080-6d9139c2-c5d8-470f-82dd-b6931fd48be0.cs-us-central1-pits.cloudshell.dev",
+        //"webpack-dev-server/client",//?https://8080-6d9139c2-c5d8-470f-82dd-b6931fd48be0.cs-us-central1-pits.cloudshell.dev",
         //"webpack/hot/dev-server",
     ],
     output: {
@@ -17,17 +17,19 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/',
     },
-    devServer: {
-        static: {
+    devServer: { // this is not used anymore, for webpack-dev-server
+        hot: false, // disable
+        inline: false,
+        /*static: {
             directory: path.join(process.cwd(), 'public'),
         },
-        /*port: 8088, // different port than nodemon
+        /port: 8088, // different port than nodemon
         proxy: {
             "^/route*": {
                 target: "http://localhost:8080/route",
                 //secure: false,
             },
-        },*/
+        },/
         //hot: true,
         //liveReload: false,
         //allowedHosts: ['host.com'],
@@ -36,7 +38,7 @@ module.exports = {
         allowedHosts: "all", // wah wah DNS rebinding attacks
         client: {
             logging: 'info',
-        },
+        },*/
     },
     devtool: "source-map",
     resolve: {
@@ -63,11 +65,11 @@ module.exports = {
         ],
         // todo css minifier
     },
-    plugins: [
+    /*plugins: [
         isDev && new HotModuleReplacementPlugin(),
         //isDev && new ReactRefreshWebpackPlugin({ overlay: false }),
         isDev && new ReactRefreshWebpackPlugin({ overlay: { sockPort: 80 }}),
         //new NodemonPlugin(),
         new NodemonPlugin({ script: "index.js", ignore: ['/dist/*'], env: {NODE_ENV: 'development'}}),
-    ],
+    ],*/
 };
